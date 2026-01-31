8e1dbdbcf5005b22afde4d0b009e650bbed688c8
Abishek Ramalingam <ar8665r@gre.ac.uk>
2026-01-31 00:04:45 +0000

    PlayerHealth
    HealthScript

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float fullHealth;
+    float currentHealth;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = fullHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void addDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            isDead();
+        }
+    }
+    public void isDead()
+    {
+        Destroy(gameObject);
+    }
+       
+}
